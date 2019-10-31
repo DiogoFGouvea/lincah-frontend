29fbeb283e490fd269ec0685d3b4789fcf240048
Diogo Gouvea <diogofgouvea@gmail.com>
2019-10-30 22:56:14 -0300

    Demonstração da apresentação

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
+// in src/App.js
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Admin, Resource} from 'react-admin'; // ListGuesser / EditGuesser
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { PostList, PostEdit, PostCreate } from './posts';
+import { UserList } from './users';
+import Dashboard from './Dashboard';
+import authProvider from './authProvider';
+// import dataProvider from './dataProvider';
+
+import PostIcon from '@material-ui/icons/Book';
+import UserIcon from '@material-ui/icons/Group';
+
+import jsonServerProvider from 'ra-data-json-server';
+
+const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
+
+const App = () => (
+    <Admin dashboard={Dashboard} 
+           authProvider={authProvider} 
+           dataProvider={dataProvider}>               
+        <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon} />
+        <Resource name="users" list={UserList} icon={UserIcon} />
+    </Admin>
+);
 
 export default App;
---
+++ src/Dashboard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardHeader from '@material-ui/core/CardHeader';
+
+export default () => (
+    <Card>
+        <CardHeader title="Lincah" />
+        <CardContent>Sistema para gestão de projetos com base em Scrum</CardContent>
+    </Card>
+);
